0d7ac3b6888d36813bdf825f54d44f1622372a53
thuannq <mscchannel02@gmail.com>
2023-03-25 23:26:14 +0700

    1

--- js/daex.js
+++ js/daex.js
@@ -208,3 +208,70 @@ $(function() {
 	
 	
 });
+
+
+
+
+
+// let context, analyser, dataArray;
+// const visualizer = document.getElementById('visualizer');
+// const togglePlayBtn = document.getElementById('toggle-play');
+
+// const audio = document.getElementById('audio');
+// audio.crossOrigin = "anonymous";
+
+// const togglePlayer = () => {
+// 	if(!context){
+// 		preparation();
+// 	}
+// 	if(audio.paused){
+// 		playAudio();
+// 	} else {
+// 		pauseAudio();
+// 	}
+// }
+
+// const playAudio = () => {
+// 	audio.play();
+// 	loop();
+// 	togglePlayBtn.innerHTML = `<svg viewBox="0 0 448 512" title="pause">
+//             <path d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z" />
+//         </svg>`;
+// }
+
+// const pauseAudio = () => {
+// 	audio.pause();
+// 	togglePlayBtn.innerHTML = `<svg viewBox="0 0 448 512" title="play">
+//             <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+//         </svg>`;
+// }
+
+// const preparation = () => {
+// 	context = new AudioContext();
+// 	analyser = context.createAnalyser();
+// 	const src = context.createMediaElementSource(audio);
+// 	src.connect(analyser);
+// 	analyser.connect(context.destination);
+// 	loop();
+// }
+
+// const loop = () => {
+	
+// 	if(audio.paused){
+// 		return;
+// 	}
+
+// 	window.requestAnimationFrame(loop);
+
+// 	dataArray = new Uint8Array(analyser.frequencyBinCount);
+// 	analyser.getByteFrequencyData(dataArray);
+// 	changeTracks();
+// }
+
+// const changeTracks = () => {
+// 	for(let i = 0; i < visualizer.children.length; i++){
+// 		visualizer.children[i].style.height = `${dataArray[i * 5] *0.39215686274}%`;
+// 	}
+// }
+
+// togglePlayBtn.addEventListener('click', togglePlayer);
